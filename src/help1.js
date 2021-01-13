dc6d6ea093cdc810db4a049e4640ebca35010e59
indra007s20 <76830399+indra007s20@users.noreply.github.com>
2021-01-13 18:15:22 -0300

    Add files via upload

---
+++ src/help1.js
@@ -0,0 +1,16 @@
+const help1 = (prefix) => {
+
+	return `
+╭────────────────╮
+ *COMANDOS DO BOT PAIN NOVOS*
+╰────────────────╯
+════════════════════
+➸ *${prefix}regras*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde* 
+════════════════════`
+}
+
+exports.help1 = help1
+
